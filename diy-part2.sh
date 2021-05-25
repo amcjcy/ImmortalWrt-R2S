1932a89d2f579d354fd4b462e46eb3287160cb96
amcjcy <82363754+amcjcy@users.noreply.github.com>
2021-05-25 14:56:47 +0800

    Add files via upload

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
-
-# Add OpenClash
-git clone --depth=1 -b master https://github.com/vernesong/OpenClash
